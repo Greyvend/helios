tmacc/agentdynamo

feat: added configurable base directory. (#826)
Co-authored-by: Mohammed Muzammil Anwar <muzammil@pluginhive.com>
Co-authored-by: Julius Marminge <julius0216@outlook.com>

apps/desktop/src/main.ts
@@ -56,8 +56,8 @@ const UPDATE_STATE_CHANNEL = "desktop:update-state";
 const UPDATE_GET_STATE_CHANNEL = "desktop:update-get-state";
 const UPDATE_DOWNLOAD_CHANNEL = "desktop:update-download";
 const UPDATE_INSTALL_CHANNEL = "desktop:update-install";
-const STATE_DIR =
-  process.env.T3CODE_STATE_DIR?.trim() || Path.join(OS.homedir(), ".t3", "userdata");
+const BASE_DIR = process.env.T3CODE_HOME?.trim() || Path.join(OS.homedir(), ".t3");
+const STATE_DIR = Path.join(BASE_DIR, "userdata");
 const DESKTOP_SCHEME = "t3";
 const ROOT_DIR = Path.resolve(__dirname, "../../..");
 const isDevelopment = Boolean(process.env.VITE_DEV_SERVER_URL);
@@ -924,7 +924,7 @@ function backendEnv(): NodeJS.ProcessEnv {
     T3CODE_MODE: "desktop",
     T3CODE_NO_BROWSER: "1",
     T3CODE_PORT: String(backendPort),
-    T3CODE_STATE_DIR: STATE_DIR,
+    T3CODE_HOME: BASE_DIR,
     T3CODE_AUTH_TOKEN: backendAuthToken,
   };
 }

apps/server/src/attachmentStore.test.ts
@@ -44,34 +44,32 @@ describe("attachmentStore", () => {
   });
 
   it("resolves attachment path by id using the extension that exists on disk", () => {
-    const stateDir = fs.mkdtempSync(path.join(os.tmpdir(), "t3code-attachment-store-"));
+    const attachmentsDir = fs.mkdtempSync(path.join(os.tmpdir(), "t3code-attachment-store-"));
     try {
       const attachmentId = "thread-1-attachment";
-      const attachmentsDir = path.join(stateDir, "attachments");
-      fs.mkdirSync(attachmentsDir, { recursive: true });
       const pngPath = path.join(attachmentsDir, `${attachmentId}.png`);
       fs.writeFileSync(pngPath, Buffer.from("hello"));
 
       const resolved = resolveAttachmentPathById({
-        stateDir,
+        attachmentsDir,
         attachmentId,
       });
       expect(resolved).toBe(pngPath);
     } finally {
-      fs.rmSync(stateDir, { recursive: true, force: true });
+      fs.rmSync(attachmentsDir, { recursive: true, force: true });
     }
   });
 
   it("returns null when no attachment file exists for the id", () => {
-    const stateDir = fs.mkdtempSync(path.join(os.tmpdir(), "t3code-attachment-store-"));
+    const attachmentsDir = fs.mkdtempSync(path.join(os.tmpdir(), "t3code-attachment-store-"));
     try {
       const resolved = resolveAttachmentPathById({
-        stateDir,
+        attachmentsDir,
         attachmentId: "thread-1-missing",
       });
       expect(resolved).toBeNull();
     } finally {
-      fs.rmSync(stateDir, { recursive: true, force: true });
+      fs.rmSync(attachmentsDir, { recursive: true, force: true });
     }
   });
 });

apps/server/src/keybindings.test.ts
@@ -3,7 +3,7 @@ import * as NodeServices from "@effect/platform-node/NodeServices";
 import { assert, it } from "@effect/vitest";
 import { assertFailure } from "@effect/vitest/utils";
 import { Effect, FileSystem, Layer, Logger, Path, Schema } from "effect";
-import { ServerConfig, type ServerConfigShape } from "./config";
+import { ServerConfig } from "./config";
 
 import {
   DEFAULT_KEYBINDINGS,
@@ -17,21 +17,17 @@ import {
 } from "./keybindings";
 
 const KeybindingsConfigJson = Schema.fromJsonString(KeybindingsConfig);
-const makeKeybindingsLayer = () =>
-  KeybindingsLive.pipe(
+const makeKeybindingsLayer = () => {
+  return KeybindingsLive.pipe(
     Layer.provideMerge(
-      Layer.effect(
-        ServerConfig,
-        Effect.gen(function* () {
-          const fs = yield* FileSystem.FileSystem;
-          const { join } = yield* Path.Path;
-          const dir = yield* fs.makeTempDirectoryScoped({ prefix: "t3code-server-config-test-" });
-          const configPath = join(dir, "keybindings.json");
-          return { keybindingsConfigPath: configPath } as ServerConfigShape;
+      Layer.fresh(
+        ServerConfig.layerTest(process.cwd(), {
+          prefix: "t3code-keybindings-test-",
         }),
       ),
     ),
   );
+};
 
 const toDetailResult = <A, R>(effect: Effect.Effect<A, KeybindingsConfigError, R>) =>
   effect.pipe(
@@ -42,7 +38,9 @@ const toDetailResult = <A, R>(effect: Effect.Effect<A, KeybindingsConfigError, R
 const writeKeybindingsConfig = (configPath: string, rules: readonly KeybindingRule[]) =>
   Effect.gen(function* () {
     const fileSystem = yield* FileSystem.FileSystem;
+    const path = yield* Path.Path;
     const encoded = yield* Schema.encodeEffect(KeybindingsConfigJson)(rules);
+    yield* fileSystem.makeDirectory(path.dirname(configPath), { recursive: true });
     yield* fileSystem.writeFileString(configPath, encoded);
   });
 

scripts/dev-runner.test.ts
@@ -1,10 +1,10 @@
 import * as NodeServices from "@effect/platform-node/NodeServices";
+import { homedir } from "node:os";
 import { resolve } from "node:path";
 import { assert, describe, it } from "@effect/vitest";
 import { Effect } from "effect";
 
 import {
-  DEFAULT_DEV_STATE_DIR,
   createDevRunnerEnv,
   findFirstAvailableOffset,
   resolveModePortOffsets,
@@ -46,27 +46,24 @@ it.layer(NodeServices.layer)("dev-runner", (it) => {
   });
 
   describe("createDevRunnerEnv", () => {
-    it.effect("defaults state dir to ~/.t3/dev when not provided", () =>
+    it.effect("defaults T3CODE_HOME to ~/.t3 when not provided", () =>
       Effect.gen(function* () {
-        const [env, defaultStateDir] = yield* Effect.all([
-          createDevRunnerEnv({
-            mode: "dev",
-            baseEnv: {},
-            serverOffset: 0,
-            webOffset: 0,
-            stateDir: undefined,
-            authToken: undefined,
-            noBrowser: undefined,
-            autoBootstrapProjectFromCwd: undefined,
-            logWebSocketEvents: undefined,
-            host: undefined,
-            port: undefined,
-            devUrl: undefined,
-          }),
-          DEFAULT_DEV_STATE_DIR,
-        ]);
+        const env = yield* createDevRunnerEnv({
+          mode: "dev",
+          baseEnv: {},
+          serverOffset: 0,
+          webOffset: 0,
+          t3Home: undefined,
+          authToken: undefined,
+          noBrowser: undefined,
+          autoBootstrapProjectFromCwd: undefined,
+          logWebSocketEvents: undefined,
+          host: undefined,
+          port: undefined,
+          devUrl: undefined,
+        });
 
-        assert.equal(env.T3CODE_STATE_DIR, defaultStateDir);
+        assert.equal(env.T3CODE_HOME, resolve(homedir(), ".t3"));
       }),
     );
 
@@ -77,7 +74,7 @@ it.layer(NodeServices.layer)("dev-runner", (it) => {
           baseEnv: {},
           serverOffset: 0,
           webOffset: 0,
-          stateDir: "/tmp/override-state",
+          t3Home: "/tmp/custom-t3",
           authToken: "secret",
           noBrowser: true,
           autoBootstrapProjectFromCwd: false,
@@ -87,7 +84,7 @@ it.layer(NodeServices.layer)("dev-runner", (it) => {
           devUrl: new URL("http://localhost:7331"),
         });
 
-        assert.equal(env.T3CODE_STATE_DIR, resolve("/tmp/override-state"));
+        assert.equal(env.T3CODE_HOME, resolve("/tmp/custom-t3"));
         assert.equal(env.T3CODE_PORT, "4222");
         assert.equal(env.VITE_WS_URL, "ws://localhost:4222");
         assert.equal(env.T3CODE_NO_BROWSER, "1");
@@ -107,7 +104,7 @@ it.layer(NodeServices.layer)("dev-runner", (it) => {
           },
           serverOffset: 0,
           webOffset: 0,
-          stateDir: undefined,
+          t3Home: undefined,
           authToken: undefined,
           noBrowser: undefined,
           autoBootstrapProjectFromCwd: undefined,
@@ -129,7 +126,7 @@ it.layer(NodeServices.layer)("dev-runner", (it) => {
           baseEnv: {},
           serverOffset: 0,
           webOffset: 0,
-          stateDir: undefined,
+          t3Home: undefined,
           authToken: undefined,
           noBrowser: undefined,
           autoBootstrapProjectFromCwd: undefined,
@@ -140,6 +137,27 @@ it.layer(NodeServices.layer)("dev-runner", (it) => {
         });
 
         assert.equal(env.T3CODE_LOG_WS_EVENTS, "0");
+      }),
+    );
+
+    it.effect("uses custom t3Home when provided", () =>
+      Effect.gen(function* () {
+        const env = yield* createDevRunnerEnv({
+          mode: "dev",
+          baseEnv: {},
+          serverOffset: 0,
+          webOffset: 0,
+          t3Home: "/tmp/my-t3",
+          authToken: undefined,
+          noBrowser: undefined,
+          autoBootstrapProjectFromCwd: undefined,
+          logWebSocketEvents: undefined,
+          host: undefined,
+          port: undefined,
+          devUrl: undefined,
+        });
+
+        assert.equal(env.T3CODE_HOME, resolve("/tmp/my-t3"));
       }),
     );
   });

Greyvend/helios

apps/desktop/src/main.ts
@@ -56,12 +56,12 @@ const UPDATE_STATE_CHANNEL = "desktop:update-state";
 const UPDATE_GET_STATE_CHANNEL = "desktop:update-get-state";
 const UPDATE_DOWNLOAD_CHANNEL = "desktop:update-download";
 const UPDATE_INSTALL_CHANNEL = "desktop:update-install";
-const STATE_DIR =
-  process.env.HELIOS_STATE_DIR?.trim() || Path.join(OS.homedir(), ".helios", "userdata");
+const BASE_DIR = process.env.HELIOS_HOME?.trim() || Path.join(OS.homedir(), ".helios");
+const STATE_DIR = Path.join(BASE_DIR, "userdata");
 const DESKTOP_SCHEME = "helios";
 const ROOT_DIR = Path.resolve(__dirname, "../../..");
 const isDevelopment = Boolean(process.env.VITE_DEV_SERVER_URL);
-const APP_DISPLAY_NAME = isDevelopment ? "Helios (Dev)" : "Helios";
+const APP_DISPLAY_NAME = isDevelopment ? "Helios (Dev)" : "Helios (Alpha)";
 const APP_USER_MODEL_ID = "com.helios-dev.helios";
 const USER_DATA_DIR_NAME = isDevelopment ? "helios-dev" : "helios";
 const LEGACY_USER_DATA_DIR_NAME = isDevelopment ? "Helios (Dev)" : "Helios (Alpha)";
@@ -924,7 +924,7 @@ function backendEnv(): NodeJS.ProcessEnv {
     HELIOS_MODE: "desktop",
     HELIOS_NO_BROWSER: "1",
     HELIOS_PORT: String(backendPort),
-    HELIOS_STATE_DIR: STATE_DIR,
+    HELIOS_HOME: BASE_DIR,
     HELIOS_AUTH_TOKEN: backendAuthToken,
   };
 }

apps/server/src/attachmentStore.test.ts
@@ -44,34 +44,32 @@ describe("attachmentStore", () => {
   });
 
   it("resolves attachment path by id using the extension that exists on disk", () => {
-    const stateDir = fs.mkdtempSync(path.join(os.tmpdir(), "helios-attachment-store-"));
+    const attachmentsDir = fs.mkdtempSync(path.join(os.tmpdir(), "helios-attachment-store-"));
     try {
       const attachmentId = "thread-1-attachment";
-      const attachmentsDir = path.join(stateDir, "attachments");
-      fs.mkdirSync(attachmentsDir, { recursive: true });
       const pngPath = path.join(attachmentsDir, `${attachmentId}.png`);
       fs.writeFileSync(pngPath, Buffer.from("hello"));
 
       const resolved = resolveAttachmentPathById({
-        stateDir,
+        attachmentsDir,
         attachmentId,
       });
       expect(resolved).toBe(pngPath);
     } finally {
-      fs.rmSync(stateDir, { recursive: true, force: true });
+      fs.rmSync(attachmentsDir, { recursive: true, force: true });
     }
   });
 
   it("returns null when no attachment file exists for the id", () => {
-    const stateDir = fs.mkdtempSync(path.join(os.tmpdir(), "helios-attachment-store-"));
+    const attachmentsDir = fs.mkdtempSync(path.join(os.tmpdir(), "helios-attachment-store-"));
     try {
       const resolved = resolveAttachmentPathById({
-        stateDir,
+        attachmentsDir,
         attachmentId: "thread-1-missing",
       });
       expect(resolved).toBeNull();
     } finally {
-      fs.rmSync(stateDir, { recursive: true, force: true });
+      fs.rmSync(attachmentsDir, { recursive: true, force: true });
     }
   });
 });

apps/server/src/keybindings.test.ts
@@ -3,7 +3,7 @@ import * as NodeServices from "@effect/platform-node/NodeServices";
 import { assert, it } from "@effect/vitest";
 import { assertFailure } from "@effect/vitest/utils";
 import { Effect, FileSystem, Layer, Logger, Path, Schema } from "effect";
-import { ServerConfig, type ServerConfigShape } from "./config";
+import { ServerConfig } from "./config";
 
 import {
   DEFAULT_KEYBINDINGS,
@@ -17,21 +17,17 @@ import {
 } from "./keybindings";
 
 const KeybindingsConfigJson = Schema.fromJsonString(KeybindingsConfig);
-const makeKeybindingsLayer = () =>
-  KeybindingsLive.pipe(
+const makeKeybindingsLayer = () => {
+  return KeybindingsLive.pipe(
     Layer.provideMerge(
-      Layer.effect(
-        ServerConfig,
-        Effect.gen(function* () {
-          const fs = yield* FileSystem.FileSystem;
-          const { join } = yield* Path.Path;
-          const dir = yield* fs.makeTempDirectoryScoped({ prefix: "helios-server-config-test-" });
-          const configPath = join(dir, "keybindings.json");
-          return { keybindingsConfigPath: configPath } as ServerConfigShape;
+      Layer.fresh(
+        ServerConfig.layerTest(process.cwd(), {
+          prefix: "helios-keybindings-test-",
         }),
       ),
     ),
   );
+};
 
 const toDetailResult = <A, R>(effect: Effect.Effect<A, KeybindingsConfigError, R>) =>
   effect.pipe(
@@ -42,7 +38,9 @@ const toDetailResult = <A, R>(effect: Effect.Effect<A, KeybindingsConfigError, R
 const writeKeybindingsConfig = (configPath: string, rules: readonly KeybindingRule[]) =>
   Effect.gen(function* () {
     const fileSystem = yield* FileSystem.FileSystem;
+    const path = yield* Path.Path;
     const encoded = yield* Schema.encodeEffect(KeybindingsConfigJson)(rules);
+    yield* fileSystem.makeDirectory(path.dirname(configPath), { recursive: true });
     yield* fileSystem.writeFileString(configPath, encoded);
   });
 

scripts/dev-runner.test.ts
@@ -1,10 +1,10 @@
 import * as NodeServices from "@effect/platform-node/NodeServices";
+import { homedir } from "node:os";
 import { resolve } from "node:path";
 import { assert, describe, it } from "@effect/vitest";
 import { Effect } from "effect";
 
 import {
-  DEFAULT_DEV_STATE_DIR,
   createDevRunnerEnv,
   findFirstAvailableOffset,
   resolveModePortOffsets,
@@ -46,27 +46,24 @@ it.layer(NodeServices.layer)("dev-runner", (it) => {
   });
 
   describe("createDevRunnerEnv", () => {
-    it.effect("defaults state dir to ~/.helios/dev when not provided", () =>
+    it.effect("defaults HELIOS_HOME to ~/.t3 when not provided", () =>
       Effect.gen(function* () {
-        const [env, defaultStateDir] = yield* Effect.all([
-          createDevRunnerEnv({
-            mode: "dev",
-            baseEnv: {},
-            serverOffset: 0,
-            webOffset: 0,
-            stateDir: undefined,
-            authToken: undefined,
-            noBrowser: undefined,
-            autoBootstrapProjectFromCwd: undefined,
-            logWebSocketEvents: undefined,
-            host: undefined,
-            port: undefined,
-            devUrl: undefined,
-          }),
-          DEFAULT_DEV_STATE_DIR,
-        ]);
+        const env = yield* createDevRunnerEnv({
+          mode: "dev",
+          baseEnv: {},
+          serverOffset: 0,
+          webOffset: 0,
+          heliosHome: undefined,
+          authToken: undefined,
+          noBrowser: undefined,
+          autoBootstrapProjectFromCwd: undefined,
+          logWebSocketEvents: undefined,
+          host: undefined,
+          port: undefined,
+          devUrl: undefined,
+        });
 
-        assert.equal(env.HELIOS_STATE_DIR, defaultStateDir);
+        assert.equal(env.HELIOS_HOME, resolve(homedir(), ".helios"));
       }),
     );
 
@@ -77,7 +74,7 @@ it.layer(NodeServices.layer)("dev-runner", (it) => {
           baseEnv: {},
           serverOffset: 0,
           webOffset: 0,
-          stateDir: "/tmp/override-state",
+          heliosHome: "/tmp/custom-t3",
           authToken: "secret",
           noBrowser: true,
           autoBootstrapProjectFromCwd: false,
@@ -87,7 +84,7 @@ it.layer(NodeServices.layer)("dev-runner", (it) => {
           devUrl: new URL("http://localhost:7331"),
         });
 
-        assert.equal(env.HELIOS_STATE_DIR, resolve("/tmp/override-state"));
+        assert.equal(env.HELIOS_HOME, resolve("/tmp/custom-t3"));
         assert.equal(env.HELIOS_PORT, "4222");
         assert.equal(env.VITE_WS_URL, "ws://localhost:4222");
         assert.equal(env.HELIOS_NO_BROWSER, "1");
@@ -107,7 +104,7 @@ it.layer(NodeServices.layer)("dev-runner", (it) => {
           },
           serverOffset: 0,
           webOffset: 0,
-          stateDir: undefined,
+          heliosHome: undefined,
           authToken: undefined,
           noBrowser: undefined,
           autoBootstrapProjectFromCwd: undefined,
@@ -129,7 +126,7 @@ it.layer(NodeServices.layer)("dev-runner", (it) => {
           baseEnv: {},
           serverOffset: 0,
           webOffset: 0,
-          stateDir: undefined,
+          heliosHome: undefined,
           authToken: undefined,
           noBrowser: undefined,
           autoBootstrapProjectFromCwd: undefined,
@@ -140,6 +137,27 @@ it.layer(NodeServices.layer)("dev-runner", (it) => {
         });
 
         assert.equal(env.HELIOS_LOG_WS_EVENTS, "0");
+      }),
+    );
+
+    it.effect("uses custom heliosHome when provided", () =>
+      Effect.gen(function* () {
+        const env = yield* createDevRunnerEnv({
+          mode: "dev",
+          baseEnv: {},
+          serverOffset: 0,
+          webOffset: 0,
+          heliosHome: "/tmp/my-t3",
+          authToken: undefined,
+          noBrowser: undefined,
+          autoBootstrapProjectFromCwd: undefined,
+          logWebSocketEvents: undefined,
+          host: undefined,
+          port: undefined,
+          devUrl: undefined,
+        });
+
+        assert.equal(env.HELIOS_HOME, resolve("/tmp/my-t3"));
       }),
     );
   });